dba6c2025b145fbfee47f20e35ab3259d8ede106
Ramoun <omartarekramoun@gmail.com>
2020-08-11 05:26:22 -0400

    stuck in ids.py file with some code

--- programming_terms/ids.py
+++ programming_terms/ids.py
@@ -1,14 +1,32 @@
 # %%
-a = 'ab-c'
-b = 'ab-c'
-print(a is b) # False
-print(a == b)
+
+# strings work different than nums (remember)
+# nums are created and then all have the same id but strings have different ids even if their values are the same.
+# == -> for comapring values
+# is -> for comapring ids
+# in java -> use isequal() 
+
+a = 'ab-c' #3
+b = 'ab-c' #3
+print('a is b : ' + str(a is b)) # False
+print('a == b : ' + str(a == b))
+
 print(hex(id(a)))
 print(hex(id(b)))
-# a[0] is b[0], a[1] is b[1], a[2] is b[2],  a[3] is b[3] #(True, True, True, True)
 
+print()
+print(a[0] is b[0]) #True
+print(a[1] is b[1]) #True
+print(a[2] is b[2]) #True
+print(a[3] is b[3]) #True
 
+print()
+print(hex(id(a[0])))
+print(hex(id(b[0])))
 
+# this is has to do with the language itself
+
+# %%
 
 # >>> a='abc';b='xyz'
 # >>> a is 'abc'
